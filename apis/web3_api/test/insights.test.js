ec04746be0db2ed1baa4a08a5adaf17121bff3f6
joegasewicz <joegasewicz@gmail.com>
2022-07-25 19:01:06 +0100

    tests

--- apis/web3_api/test/insights.test.js
+++ apis/web3_api/test/insights.test.js
@@ -34,7 +34,17 @@ beforeEach(async () => {
 
 
 describe("Insights", () => {
-    it("deploys a contract", () => {
-        console.log(insights);
+    it("deploys a contract", async () => {
+        const organisation = await insights.methods.organisation().call();
+        const recipient_name = await insights.methods.recipient_name().call();
+        assert.equal(organisation, "Shell");
+        assert.equal(recipient_name, "Joe 1");
+    });
+    it("can change the release_date", async () => {
+         await insights.methods.setReleaseDate("Tomorrow").send({
+            from: accounts[0],
+         });
+        const release_date = await insights.methods.release_date().call();
+        assert.equal(release_date, "Tomorrow");
     });
 });
