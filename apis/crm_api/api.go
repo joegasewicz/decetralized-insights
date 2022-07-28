996a568ef3d597c3acac250c906bad5818f7e6cd
joegasewicz <joegasewicz@gmail.com>
2022-07-28 19:50:29 +0100

    crm templates

--- apis/crm_api/api.go
+++ apis/crm_api/api.go
@@ -4,6 +4,7 @@ import (
 	"github.com/joegasewicz/decetralized-insights/crm_api/views"
 	"github.com/joegasewicz/gomek"
 	"log"
+	"net/http"
 )
 
 const (
@@ -15,6 +16,7 @@ func main() {
 	//journeyViews := views.Journey{}
 	indexView := views.Index{}
 	log.Printf("Starting Server on %d\n", port)
+
 	// Gomek
 	app := gomek.New(gomek.Config{
 		BaseTemplateName: "layout",
@@ -22,11 +24,19 @@ func main() {
 			"./templates/layout.gohtml",
 		},
 	})
+
+	// Static ifles
+	distFiles := http.FileServer(http.Dir("dist"))
+	app.Handle("/dist/", http.StripPrefix("/dist/", distFiles))
+
+	// Routes
 	app.Route("/").View(indexView.Get).Methods("GET").Templates(
 		"./templates/routes/index.gohtml",
 	)
+	// Middleware
 	app.Use(gomek.Logging)
 	//app.Use(gomek.CORS)
+
 	app.Listen(port)
 	app.Start()
 }
