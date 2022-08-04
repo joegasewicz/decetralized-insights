3634f639593cdd708764795e67df60a4288151d8
joegasewicz <joegasewicz@gmail.com>
2022-08-04 19:34:39 +0100

    contract1

--- apis/journeys_api/contracts/Insights.sol
+++ apis/journeys_api/contracts/Insights.sol
@@ -11,6 +11,7 @@ pragma solidity >=0.4.0 <0.9.0;
     struct Recipient {
         uint id;
         string name;
+        address addr;
     }
 
 
@@ -29,11 +30,11 @@ pragma solidity >=0.4.0 <0.9.0;
 
 
 contract Journey {
-    Activity[] activities;
-    Organisation organisation;
-    Item newItem;
-    Recipient recipient;
-    Activity new_activity;
+    Activity[] public activities;
+    Organisation public organisation;
+    Item public newItem;
+    Recipient public recipient;
+    Activity public new_activity;
 
     constructor(
         uint  _item_id,
@@ -53,16 +54,17 @@ contract Journey {
 
         recipient.id = _recipient_id;
         recipient.name = _recipient_name;
+        recipient.addr = msg.sender;
 
         new_activity.id = _activity_id;
         new_activity.item = newItem;
         new_activity.act_type = _activity_type;
         new_activity.description = _activity_description;
         new_activity.recipient = recipient;
-        addNewActivity();
     }
 
     function addNewActivityToJourney() public virtual {
+        require(msg.sender == recipient.addr);
         activities.push(new_activity);
     }
 
