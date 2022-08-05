6657247c531f99b75b5401b71c8d8be458805813
joegasewicz <joegasewicz@gmail.com>
2022-08-05 18:22:54 +0100

    contract1

--- apis/journeys_api/contracts/Insights.sol
+++ apis/journeys_api/contracts/Insights.sol
@@ -1,8 +1,10 @@
-pragma solidity >=0.4.0 <0.9.0;
+pragma solidity ^0.8;
 //SPDX-License-Identifier: UNLICENSED
 
+// All ids must be of type string
+
     struct Item {
-        uint id;
+        string id;
         string name;
         string it_type;
         string description;
@@ -23,51 +25,93 @@ pragma solidity >=0.4.0 <0.9.0;
         Recipient recipient;
     }
 
-    struct Organisation {
-        uint id;
-        string name;
+    struct Insight {
+        string id;
+        Activity[] activities;
+        Item item;
     }
 
+library MappingUtils {
 
-contract Journey {
-    uint id; // should be a uuid
-    Activity[] public activities;
-    Organisation public organisation;
-    Item public newItem;
-    Recipient public recipient;
-    Activity public new_activity;
+    function keyExists(string memory key, mapping(string => Insight) storage map) private view returns (bool) {
+        if (bytes(map[key].id).length > 0) {
+            return false;
+        }
+        return true;
+    }
+}
+
+contract Organisation {
+    using MappingUtils for *;
+    uint public id;
+    string public name;
+
+    Recipient[] public recipients;
+    mapping(string => Insight) public insights;
+    address admin_addr;
+    address[] user_addrs;
 
     constructor(
         uint org_id,
         string memory org_name
     )  {
-        organisation.id = org_id;
-        organisation.name = org_name;
+        id = org_id;
+        name = org_name;
     }
 
-    function removeActivity(uint activity_id) public virtual {
-        require(msg.sender == recipient.addr);
-        for (uint i; i < activities.length; i++) {
-            if (activities[i].id == activity_id) {
-                delete activities[i];
-                return;
-            }
-        }
+    // function removeActivity(uint activity_id) public virtual {
+    //     require(msg.sender == recipient.addr);
+    //     for (uint i; i < activities.length; i++) {
+    //         if (activities[i].id == activity_id) {
+    //             delete activities[i];
+    //             return;
+    //         }
+    //     }
+    // }
+
+    // function updateActivity(uint activity_id, Activity memory activity) public virtual {
+    //     require(msg.sender == recipient.addr);
+    //     for (uint i; i < activities.length; i++) {
+    //         if (activities[i].id == activity_id) {
+    //             activities[i] = activity;
+    //             return;
+    //         }
+    //     }
+    // }
+
+    // function createInsightID(uint item_id) private view returns (string memory) {
+    //     return string(abi.encodePacked(id, item_id));
+    // }
+
+    function addNewInsight(
+        string memory item_id,
+        string memory item_name,
+        string memory it_type,
+        string memory item_description
+    ) private {
+        // require(msg.sender == recipients[0].addr);
+
+        Insight memory insight;
+        Activity[] memory activities;
+        Item memory item;
+
+        item.id = item_id;
+        item.name = item_name;
+        item.it_type = it_type;
+        item.description = item_description;
+        // Check for duplicate values
+        insights[item.id] = insight;
+        insight.activities = activities;
+        insight.item = item;
     }
 
-    function updateActivity(uint activity_id, Activity memory activity) public virtual {
-        require(msg.sender == recipient.addr);
-        for (uint i; i < activities.length; i++) {
-            if (activities[i].id == activity_id) {
-                activities[i] = activity;
-                return;
-            }
-        }
-    }
+    // function getInsight(uint insight_id) public view return (Insight memory) {
+
+    // }
 
 
     function addNewActivity (
-        uint  _item_id,
+        string memory  _item_id,
         string memory _item_name,
         string memory _item_type,
         string memory _item_description,
@@ -77,6 +121,10 @@ contract Journey {
         string memory _activity_type,
         string memory _activity_description
     ) public virtual {
+        Item memory newItem;
+        Activity memory new_activity;
+        Recipient memory recipient;
+
         newItem.id = _item_id;
         newItem.name = _item_name;
         newItem.it_type = _item_type;
@@ -91,7 +139,10 @@ contract Journey {
         new_activity.act_type = _activity_type;
         new_activity.description = _activity_description;
         new_activity.recipient = recipient;
-        activities.push(new_activity);
+        // Check insight exists
+
+        // Add new insight to contract
+        insights[_item_id].activities.push(new_activity);
     }
 
 }
